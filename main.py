fb0b1fec59c6ea5098d3d33998a1a47d9a1522ef
Brady Wales <bradywales@gmail.com>
2021-11-06 18:55:24 -0400

    Working reddit as list

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import
 from cmu_112_graphics import *
+import time
 
 def reddit():
     driver = webdriver.Chrome("../hack112/chromedriver")
     url = 'https://www.reddit.com/'
     driver.get(url)
-    html =  BeautifulSoup(driver.page_source, "html.parser")
     print(driver.current_url)
     while(driver.current_url == "https://www.reddit.com/login/"):
         print("Logging in")
     print("logged in")
     html =  BeautifulSoup(driver.page_source, "html.parser")
     result = html.find_all("h3",{"class":"_eYtD2XCVieq6emjKBH3m"})
+    results = []
     for item in result:
         print(item.text)
+        results.append(item.text)
+    return(results)
+        
+    
 
 print(repr(reddit()))
