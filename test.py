c9545046983fe5ad67089e470cbd2e0065e4784f
Brady Wales <bradywales@gmail.com>
2021-11-06 18:34:46 -0400

    REDDIT LOGIN GETS TITLES SUCCESS

--- test.py
+++ test.py
@@ -4,8 +4,16 @@ from selenium import webdriver
 import time
 
 driver = webdriver.Chrome("../hack112/chromedriver")
-url = 'https://www.reddit.com/'
+url = 'https://www.reddit.com/login'
 driver.get(url)
+html =  BeautifulSoup(driver.page_source, "html.parser")
+
+print(driver.current_url)
+
+while(driver.current_url == "https://www.reddit.com/login/"):
+    print("Logging in")
+print("logged in")
+
 html =  BeautifulSoup(driver.page_source, "html.parser")
 result = html.find_all("h3",{"class":"_eYtD2XCVieq6emjKBH3m"})
 for item in result:
