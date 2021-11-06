36e6ffbc3a9d154ea263809e842b07f84537024f
Brady Wales <bradywales@gmail.com>
2021-11-06 18:57:44 -0400

    Working reddit

--- twitter.py
+++ twitter.py
@@ -4,17 +4,20 @@ from selenium import webdriver
 import time
 
 driver = webdriver.Chrome("../hack112/chromedriver")
-url = 'https://www.reddit.com/login'
+url = 'https://www.twitter.com/login'
 driver.get(url)
 
 print(driver.current_url)
 
-while(driver.current_url == "https://www.reddit.com/login/"):
+while(driver.current_url != "https://twitter.com/home"):
     print("Logging in")
 print("logged in")
 
 html =  BeautifulSoup(driver.page_source, "html.parser")
-result = html.find_all("h3",{"class":"_eYtD2XCVieq6emjKBH3m"})
+result = html.find_all({"class":"css-1dbjc4n r-1iusvr4 r-16y2uox r-1777fci r-kzbkwu"})
+print(result)
 for item in result:
+    print(item)
+    content = item.find({"class":"css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"})
     print(item.text)
 
