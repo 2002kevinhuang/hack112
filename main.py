4b922e3869f76773ee01a98c5ce9bfbd099b6597
Kevin Huang <2002kevinhuang@gmail.com>
2021-11-06 18:50:27 -0400

    added 112 graphics

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print('hack112')
-print("hi")
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import
+from cmu_112_graphics import *
+
+def reddit():
+    driver = webdriver.Chrome("../hack112/chromedriver")
+    url = 'https://www.reddit.com/'
+    driver.get(url)
+    html =  BeautifulSoup(driver.page_source, "html.parser")
+    print(driver.current_url)
+    while(driver.current_url == "https://www.reddit.com/login/"):
+        print("Logging in")
+    print("logged in")
+    html =  BeautifulSoup(driver.page_source, "html.parser")
+    result = html.find_all("h3",{"class":"_eYtD2XCVieq6emjKBH3m"})
+    for item in result:
+        print(item.text)
+
+print(repr(reddit()))
