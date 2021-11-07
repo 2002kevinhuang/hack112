9b1a8e8872b73e2fd1803eaee2eb003858109298
Kevin Huang <2002kevinhuang@gmail.com>
2021-11-06 21:01:59 -0400

    moved reddit function to other file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from cmu_112_graphics import *
 import time
+from test import *
 
 
 def appStarted(app):
@@ -20,23 +21,6 @@ def redrawAll(app, canvas):
         canvas.create_text(20, y + margin, text=line, anchor=tkinter.NW, width=app.width - 50)
         margin += y
     # canvas.create_text(20, 20, text=app.reddit, anchor=tkinter.NW, width=app.width-50)
-
-
-def reddit():
-    driver = webdriver.Chrome("../hack112/chromedriver")
-    url = 'https://www.reddit.com/'
-    driver.get(url)
-    print(driver.current_url)
-    while (driver.current_url == "https://www.reddit.com/login/"):
-        print("Logging in")
-    print("success")
-    time.sleep(2)
-    html = BeautifulSoup(driver.page_source, "html.parser")
-    result = html.find_all("h3", {"class": "_eYtD2XCVieq6emjKBH3m"})
-    results = []
-    for item in result:
-        results.append(item.text)
-    return results
 
 
 # print(reddit())
--- test.py
+++ test.py
@@ -3,18 +3,18 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome("../hack112/chromedriver")
-url = 'https://www.reddit.com/login'
-driver.get(url)
-
-print(driver.current_url)
-
-while(driver.current_url == "https://www.reddit.com/login/"):
-    print("Logging in")
-print("logged in")
-
-html =  BeautifulSoup(driver.page_source, "html.parser")
-result = html.find_all("h3",{"class":"_eYtD2XCVieq6emjKBH3m"})
-for item in result:
-    print(item.text)
-
+def reddit():
+    driver = webdriver.Chrome("../hack112/chromedriver")
+    url = 'https://www.reddit.com/'
+    driver.get(url)
+    print(driver.current_url)
+    while (driver.current_url == "https://www.reddit.com/login/"):
+        print("Logging in")
+    print("success")
+    time.sleep(2)
+    html = BeautifulSoup(driver.page_source, "html.parser")
+    result = html.find_all("h3", {"class": "_eYtD2XCVieq6emjKBH3m"})
+    results = []
+    for item in result:
+        results.append(item.text)
+    return results
